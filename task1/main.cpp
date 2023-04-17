6fa175225e2e627fa8edfa5e49ff38e225e5cf80
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-04-18 02:49:43 +0300

    Update main.cpp

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,1 +1,41 @@
-
+#include <iostream>
+#include <cctype>
+#include "Cipher.h"
+using namespace std;
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {
+        wstring cipherText;
+        wstring decryptedText;
+        Cipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = tolower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        cerr<<"Error: "<<e.what()<<endl;
+    }
+}
+int main(int argc, char **argv)
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    wcout<<L'-'<<endl;
+    check(L"ТОЙОТАСУПРА",L"ХЕЛЛОУ");
+    wcout<<L'-'<<endl;
+    check(L"ТОЙОТАСУПРА",L"");
+    wcout<<L'-'<<endl;
+    check(L"ТОЙОТАСУПРА",L"Х1Е2Л3Л4О5У6");
+    wcout<<L'-'<<endl;
+    check(L"ТОЙОТА СУПРА",L"ХЕЛЛОУ");
+    wcout<<L'-'<<endl;
+    check(L"ТОЙОТАСУПРА",L"ХЕЛЛОУ",true);
+    wcout<<L'-'<<endl;
+    check(L"12345789",L"ХЕЛЛОУ");
+    wcout<<L'-'<<endl;
+    return 0;
+}
